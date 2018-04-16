b8d9dba26ec3cb30ae81792b8e99391d108f03fa
Joe Stump <joe@joestump.net>
2018-04-16 17:04:35 -0500

    Add branch filtering support.

--- aws/resource_aws_codebuild_webhook.go
+++ aws/resource_aws_codebuild_webhook.go
@@ -18,6 +18,10 @@ func resourceAwsCodeBuildWebhook() *schema.Resource {
 				Required: true,
 				ForceNew: true,
 			},
+			"branch_filter": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
 			"url": {
 				Type:     schema.TypeString,
 				Computed: true,
@@ -30,7 +34,8 @@ func resourceAwsCodeBuildWebhookCreate(d *schema.ResourceData, meta interface{})
 	conn := meta.(*AWSClient).codebuildconn
 
 	resp, err := conn.CreateWebhook(&codebuild.CreateWebhookInput{
-		ProjectName: aws.String(d.Get("name").(string)),
+		ProjectName:  aws.String(d.Get("name").(string)),
+		BranchFilter: aws.String(d.Get("branch_filter").(string)),
 	})
 	if err != nil {
 		return err
